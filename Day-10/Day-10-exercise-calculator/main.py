72a93edcd67da85f8c890d725633a0cdf816002d
Daniel Wernz <dwernz@att.net>
2021-03-23 15:13:48 -0400

    Day 10 finish

---
+++ Day-10/Day-10-exercise-calculator/main.py
@@ -0,0 +1,36 @@
+from art import logo
+print(logo)
+
+# Addition
+def add(n1, n2):
+  return n1 + n2
+
+# Subtraction
+def subtract(n1, n2):
+  return n1 - n2
+
+# Multiplication
+def multiply(n1, n2):
+  return n1 * n2
+
+# Division
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
+}
+
+num1 = int(input("What's the first number? "))
+
+operation_symbol = input("Pick an operation from the line above (+, -, *, /): ")
+
+num2 = int(input("What's the second number? "))
+
+
+answer = operations[operation_symbol](num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
